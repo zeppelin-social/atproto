dd9d7430ab5c5572d21794e08d67361a9f8fb7e6
futurGH <me@futuristick.ca>
2025-07-14 09:53:07 -0400

    zeppelin: stub age assurance endpoints

--- packages/bsky/src/api/app/bsky/unspecced/getAgeAssuranceState.ts
+++ packages/bsky/src/api/app/bsky/unspecced/getAgeAssuranceState.ts
@@ -1,46 +1,17 @@
-import { UpstreamFailureError } from '@atproto/xrpc-server'
 import { AppContext } from '../../../../context'
 import { Server } from '../../../../lexicon'
-import { ActorInfo } from '../../../../proto/bsky_pb'
 
 export default function (server: Server, ctx: AppContext) {
   server.app.bsky.unspecced.getAgeAssuranceState({
     auth: ctx.authVerifier.standard,
-    handler: async ({ auth }) => {
-      const viewer = auth.credentials.iss
-      const actorInfo = await getAgeVerificationState(ctx, viewer)
-
+    handler: async () => {
       return {
         encoding: 'application/json',
         body: {
-          lastInitiatedAt:
-            actorInfo.ageAssuranceStatus?.lastInitiatedAt
-              ?.toDate()
-              .toISOString() ?? undefined,
-          status: actorInfo.ageAssuranceStatus?.status ?? 'unknown',
+          lastInitiatedAt: '1970-01-01T00:00:00.000Z',
+          status: 'assured',
         },
       }
     },
   })
-}
-
-const getAgeVerificationState = async (
-  ctx: AppContext,
-  actorDid: string,
-): Promise<ActorInfo> => {
-  try {
-    const res = await ctx.dataplane.getActors({
-      dids: [actorDid],
-      returnAgeAssuranceForDids: [actorDid],
-      skipCacheForDids: [actorDid],
-    })
-
-    return res.actors[0]
-  } catch (err) {
-    throw new UpstreamFailureError(
-      'Cannot get current age assurance state',
-      'GetAgeAssuranceStateFailed',
-      { cause: err },
-    )
-  }
 }
